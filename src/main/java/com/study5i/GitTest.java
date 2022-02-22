3c4fb1ab8529a01372331f85ae6fe58ade22452d
DreamBackCC <80751985+DreamBackCC@users.noreply.github.com>
2022-02-22 21:28:19 +0800

    pull

--- src/main/java/com/study5i/GitTest.java
+++ src/main/java/com/study5i/GitTest.java
@@ -15,6 +15,6 @@ public class GitTest {
         System.out.println("hello git5");
         System.out.println("hello master");
         System.out.println("hello hot-fix");
-        System.out.println("pull test);
+        System.out.println("pull test");
     }
 }
