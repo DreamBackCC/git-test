260ef31f1a6c2c9f01b7d0ec71b78bfe74465645
kangdu0316 <123@qq.com>
2022-02-22 19:42:52 +0800

    third commit

--- src/main/java/com/study5i/GitTest.java
+++ src/main/java/com/study5i/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
         System.out.println("hello git1");
         System.out.println("hello git2");
         System.out.println("hello git3");
+        System.out.println("hello git4");
     }
 }
