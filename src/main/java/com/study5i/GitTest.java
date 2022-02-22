d93f3c68f3fe34195c189cebc0814f1aca67c59d
kangdu0316 <123@qq.com>
2022-02-22 19:53:06 +0800

    hot-fix commit2

--- src/main/java/com/study5i/GitTest.java
+++ src/main/java/com/study5i/GitTest.java
@@ -13,5 +13,6 @@ public class GitTest {
         System.out.println("hello git3");
         System.out.println("hello git4");
         System.out.println("hello git5");
+        System.out.println("hello hot-fix");
     }
 }
