7be66179ce6dcd72008aecf1aa6c2c602686cd24
kangdu0316 <123@qq.com>
2022-02-22 19:38:27 +0800

    first commit

---
+++ src/main/java/com/study5i/GitTest.java
@@ -0,0 +1,13 @@
+package com.study5i;
+
+/**
+ * Created With IDEA
+ *
+ * @Author:JiaChenKang
+ * @Date:2022/02/22
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
